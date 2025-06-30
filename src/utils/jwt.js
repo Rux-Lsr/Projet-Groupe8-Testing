96b17523a2d7451010bc4f2d8b801cf0a9821d2c
Sophia Mba <sophiamba17@gmail.com>
2025-06-30 20:47:47 +0100

    update-utils

---
+++ src/utils/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.generateTokens = (user) => {
+  const accessToken = jwt.sign(
+    { id: user.id, email: user.email },
+    JWT_SECRET,
+    { expiresIn: "15m" } // Token court terme
+  );
+
+  const refreshToken = jwt.sign(
+    { id: user.id },
+    JWT_SECRET,
+    { expiresIn: "7d" } // Token long terme
+  );
+
+  return { accessToken, refreshToken };
+};
+
+exports.verifyToken = (token) => {
+  return jwt.verify(token, JWT_SECRET);
+};
